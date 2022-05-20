74b9f292e2763622c1b0f59657ba3d9fc78f13ce
ihab <ihab@utopios.net>
2022-05-20 16:35:33 +0200

    exemple du flat map

--- projet-j-1/src/main/java/MovieCounter.java
+++ projet-j-1/src/main/java/MovieCounter.java
@@ -1,12 +1,16 @@
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Map;
 
 public class MovieCounter {
     private static final String path = "data-spark/film.data";
     public static void main(String[] args) {
-        JavaRDD<Integer> rdd = Tools.getContext().textFile(path).map(l -> Integer.valueOf(l.split("\t")[2]));
+        JavaRDD<Integer> rdd = Tools.getContext().textFile(path)
+                //.flatMap(e -> Arrays.stream(e.split("\n")).iterator())
+                .map(l -> Integer.valueOf(l.split("\t")[2]));
         Map<Integer, Long> result = rdd.countByValue();
     }
 }
---
+++ projet-j-1/src/main/java/WordCounter.java
@@ -0,0 +1,12 @@
+import org.apache.spark.api.java.JavaRDD;
+
+import java.util.Arrays;
+
+public class WordCounter {
+
+    public static void main(String[] args) {
+        JavaRDD rdd = Tools.getContext().textFile("data-spark/book.txt").flatMap(e -> Arrays.stream(e.split(" ")).iterator()).distinct();
+
+        Long result = rdd.count();
+    }
+}
